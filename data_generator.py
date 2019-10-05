63650a9bf046dbc897bd560b8592466abae3c97e
Mert Dumenci <mert@dumenci.me>
2019-10-05 00:29:02 -0400

    Implement fast CNF

--- data_generator.py
+++ data_generator.py
@@ -12,30 +12,23 @@ import pickle
 from typing import List, Tuple
 
 import tqdm
-from satml import generator, types, expression, bounded_search
+from satml import generator, types, fast_cnf, bounded_search
 from satml.solvers import cryptominisat
 import satml.solver
 
 
 def generator_instance(packed_args):
-    num_vars, num_clauses, expansion_depth, dimacs_path = packed_args
+    dimacs_path, expansion_depth = packed_args
 
     solver = cryptominisat.Cryptominisat()
-    if dimacs_path:
-        with open(dimacs_path, 'r') as dimacs:
-            dimacs = dimacs.read()
-            f = expression.from_dimacs(dimacs)
-    else:
-        _, f, _ = generator.generate_random(3, num_vars=num_vars, num_clauses=num_clauses, solver=solver, force_satisfiable=True)
+    with open(dimacs_path, 'r') as dimacs:
+        f = dimacs.read()
 
     return bounded_search.h_star(f, solver, expansion_depth)
 
 
 parser = argparse.ArgumentParser('Generates sat-ml data')
-parser.add_argument('--sr_dimacs', type=str, help='Should we use a `neurosat` generated random formula set instead of generating it ourselves', required=False)
-parser.add_argument('--num_formulas', type=int, help='Number of formulas to generate.', required=False)
-parser.add_argument('--num_vars', type=int, help='Number of variables in random formulas.', required=False)
-parser.add_argument('--num_clauses', type=int, help='Number of clauses in random formulas.', required=False)
+parser.add_argument('--dimacs', type=str, help='Folder of formulas in `dimacs` format.')
 parser.add_argument('--expansion_depth', type=int, help='H_star expansion depth', default=5)
 parser.add_argument('--output', type=str, help='Where to write the pickled file.')
 parser.add_argument('--single_threaded', type=bool, help='Should we run in single-threaded mode or not', default=False)
@@ -45,19 +38,13 @@ args = parser.parse_args()
 
 cpu_count = 1 if args.single_threaded else multiprocessing.cpu_count()
 with multiprocessing.Pool(cpu_count) as p:
-    sr_dimacs = None
-    if args.sr_dimacs:
-        pattern = os.path.join(args.sr_dimacs, "*.dimacs")
-        sr_dimacs = list(glob.glob(pattern))
+    pattern = os.path.join(args.dimacs, "*.dimacs")
+    names = list(glob.glob(pattern))
+    
+    # Run the search
+    pool_map = p.imap(generator_instance, ((path, args.expansion_depth) for path in names))
+    results = list(tqdm.tqdm(pool_map, total=len(names)))
 
-    num_formulas = len(sr_dimacs) if not args.num_formulas else args.num_formulas
-
-    if sr_dimacs:
-        pool_map = p.imap(generator_instance, ((0, 0, args.expansion_depth, dimacs_path) for dimacs_path in sr_dimacs[:num_formulas]))
-    else:
-        pool_map = p.imap(generator_instance, ((args.num_vars, args.num_clauses, args.expansion_depth, sr_dimacs[i] if sr_dimacs else None) for i in range(args.num_formulas)))
-
-    results = list(tqdm.tqdm(pool_map, total=num_formulas))
     # Filter out unsuccessful searches
     results = [result for result in results if result is not None]
     dataset = []
--- satml/bounded_search.py
+++ satml/bounded_search.py
@@ -5,10 +5,10 @@ Author: Mert Dumenci
 
 import random
 from typing import List, Tuple
-from satml import expression, types, solver
+from satml import fast_cnf, types, solver
 
 
-def h_star(f: expression.Expression, solver: solver.Solver, depth: int, verbose=False) -> List[Tuple[expression.Expression, types.Decision]]:
+def h_star(dimacs: types.Dimacs, solver: solver.Solver, depth: int) -> List[Tuple[fast_cnf.FastCNF, types.Decision]]:
     """
     Given a formula, run an exhaustive search for a given number of levels
     improving the solver heuristic if possible.
@@ -20,72 +20,55 @@ def h_star(f: expression.Expression, solver: solver.Solver, depth: int, verbose=
 
     :pre: `f` must be satisfiable.
     """
-    current_formula = f
     current_depth = 0
 
-    sat, initial_solver_guess = solver.solve(expression.to_dimacs(f))
-    assert sat, "`f` must be satisfiable."
+    sat, initial_solver_guess = solver.solve(dimacs)
+    assert sat, "`dimacs` must be satisfiable."
+
+    current_formula = fast_cnf.from_dimacs(dimacs)
+    # Normalize variable names.
+    fast_cnf.rename_(current_formula)
 
     decisions = []
-    num_vars = max((int(v) for v in expression.free(f)))
+    num_vars = max(fast_cnf.free(current_formula))
     num_frontier_decisions = -1
-    
-    if verbose:
-        print("🎬 Starting with initial solver guess of {} decisions".format(initial_solver_guess))
 
     while current_formula and current_depth < depth:
-        if verbose:
-            print("✅ Level {}".format(current_depth))
-    
-        free_variables = list(expression.free(current_formula))
+        free_variables = list(fast_cnf.free(current_formula))
         # We don't want any internal Python set-iteration-order bias to seep in our variable selection metrics.
-        # Also, why is this in-place Python????
         random.shuffle(free_variables)
         num_frontier_decisions, best_decision, best_decision_formula = -1, None, None
-
-        if verbose:
-            print("Initial: {}".format(expression.pprint(current_formula)))
 
         # Try all assignments to all free variables, referencing the solver for the
         # quality of the decision.
         for variable in free_variables:
             for assignment in [True, False]:
-                fp = expression.simplify(expression.assign(current_formula, variable, assignment))
+                fp = fast_cnf.assign(current_formula, variable, assignment)
+            
+                # if expression.trivially_sat(fp):
+                #     sat, num_decisions = True, 0
+                # elif expression.trivially_unsat(fp):
+                #     sat, num_decisions = False, 0
+                # else:
+                sat, num_decisions = solver.solve(fast_cnf.to_dimacs(fp, num_vars=num_vars))
 
-                if expression.trivially_sat(fp):
-                    print("Trivially sat")
-                    sat, num_decisions = True, 0
-                elif expression.trivially_unsat(fp):
-                    print("Trivially unsat")
-                    sat, num_decisions = False, 0
-                else:
-                    sat, num_decisions = solver.solve(expression.to_dimacs(fp, num_vars=num_vars))
-
-                # Need to normalize the number of decisions because we make the solver "decide" for level more variables
-                # by injecting the total # of variables in the dimacs at every level. (so that deciding the last variable doesn't restrict the
-                # search space and artificially deflate the decisions in the solver.)
+                # Need to normalize the number of decisions, because every time we assign to the maximally named variable,
+                # the solver makes one less decision (because in its eyes the number of variables go down.)
+                # 
+                # Assigning 2 or 3 in (1 or 2 or 3) shouldn't change the number of decisions made, they should both output 3 (one trivial.)
+                # We do this in the above line, and just subtract the number of decisions we know we've made from the solver's output here.
                 num_decisions = max(0, num_decisions - current_depth)
-
-                if verbose and sat:
-                    print("{} {} = {}, Current Best Frontier: {}, New Frontier: {}".format("💚" if num_decisions < num_frontier_decisions else "🔴", variable, assignment, num_frontier_decisions, num_decisions))
-                    print(expression.pprint(fp))
-                    print(expression.to_dimacs(fp))
 
                 if sat and (num_frontier_decisions == -1 or num_decisions < num_frontier_decisions):
                     best_decision = current_formula, (variable, assignment)
                     num_frontier_decisions = num_decisions
                     best_decision_formula = fp
 
-        if verbose:
-            print("💈 Best decision has frontier {}".format(num_frontier_decisions))
-
         decisions.append(best_decision)
         current_formula = best_decision_formula
         current_depth += 1
 
     total_num_decisions = depth + num_frontier_decisions
-    if verbose:
-        print("Initial solver decisions: {}, found number of decisions: {}".format(initial_solver_guess, total_num_decisions))
 
     if num_frontier_decisions > 0 and total_num_decisions < initial_solver_guess:
         return decisions, initial_solver_guess, total_num_decisions
